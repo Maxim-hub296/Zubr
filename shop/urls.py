b5a5c8c08dfae6926b9c008f69399c2d47e30fc6
Maxim <maxsavikin@gmail.com>
2025-06-11 23:26:55 +0300

    Промежуточный коммит

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'shop'
 urlpatterns = [
-
+    path("", views.ProductsListView.as_view(), name="products"),
+    path("<int:pk>/", views.ProductView.as_view(), name="product")
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Product
 # Create your views here.
+
+
+class ProductsListView(ListView):
+    template_name = "shop/products_list.html"
+    context_object_name = "products"
+
+    def get_queryset(self):
+        return Product.objects.all()
+
+
+class ProductView(DetailView):
+    template_name = "shop/product.html"
+    model = Product
